e4fa8aeeeef974e84a013238927d8a7cc3f583a5
ambarish2012 <achitnis2012@gmail.com>
2017-12-12 22:00:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/properties', properties);
 // catch 404 and forward to error handler
 app.use(
   function (req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Resource Not Found');
     err.status = 404;
     next(err);
   }
